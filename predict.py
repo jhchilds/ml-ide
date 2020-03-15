54e292e4ced1ea0f0e3ced7b3a882ba59d6b0fa0
joshuachilds <jhchilds@uvm.edu>
2020-03-15 12:07:39 -0400

    adding targets to feature vector

--- predict.py
+++ predict.py
@@ -4,10 +4,16 @@ import numpy as np
 
 data_list = json.load(open("data.json"))
 corpus = [data["data"] for data in data_list]
+target = [target["target"] for target in data_list]
 
 vectorizer = CountVectorizer(analyzer="char", ngram_range=(2, 2), max_features=1000)
 vectorized_X = vectorizer.fit_transform(corpus)
 print(vectorizer.get_feature_names())
-print(vectorized_X.toarray())
+features = vectorized_X.toarray()
+target = np.asarray(target)
+target = target.reshape((len(target), 1))
 
-np.save("vectorized", vectorized_X.toarray())
+X = np.append(features, target, axis=1)
+print(X)
+np.save("vectorized", X)
+
