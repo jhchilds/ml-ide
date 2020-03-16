4af35a85708911d33ce33ec57220c5201665fe34
Sam Clark <sam.clark1142@gmail.com>
2020-03-15 20:15:21 -0400

    force balance dataset and predict with svm

---
+++ create_features.py
@@ -0,0 +1,19 @@
+from sklearn.feature_extraction.text import CountVectorizer
+import json
+import numpy as np
+
+data_list = json.load(open("data.json"))
+corpus = [data["data"] for data in data_list]
+target = [target["target"] for target in data_list]
+
+vectorizer = CountVectorizer(analyzer="char", ngram_range=(2, 2), max_features=100)
+vectorized_X = vectorizer.fit_transform(corpus)
+print(vectorizer.get_feature_names())
+features = vectorized_X.toarray()
+target = np.asarray(target)
+target = target.reshape((len(target), 1))
+
+X = np.append(features, target, axis=1)
+print(X)
+np.save("vectorized", X)
+
--- predict.py
+++ predict.py
@@ -1,19 +1,46 @@
-from sklearn.feature_extraction.text import CountVectorizer
-import json
 import numpy as np
 
-data_list = json.load(open("data.json"))
-corpus = [data["data"] for data in data_list]
-target = [target["target"] for target in data_list]
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
-vectorizer = CountVectorizer(analyzer="char", ngram_range=(2, 2), max_features=1000)
-vectorized_X = vectorizer.fit_transform(corpus)
-print(vectorizer.get_feature_names())
-features = vectorized_X.toarray()
-target = np.asarray(target)
-target = target.reshape((len(target), 1))
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
 
-X = np.append(features, target, axis=1)
-print(X)
-np.save("vectorized", X)
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import learning_curve
 
+data = np.load("vectorized.npy")
+print(data)
+print(data.shape)
+
+balanced_X = np.zeros((1000, 100))
+balanced_y = np.zeros(1000, dtype="str")
+
+counts = {}
+
+index = 0
+for row in data:
+    label = row[-1]
+    if label in counts:
+        counts[label] += 1
+    else:
+        counts[label] = 1
+
+    if counts[label] <= 500:
+        balanced_X[index] = row[:-1]
+        balanced_y[index] = row[-1]
+        index += 1
+
+print(counts)
+print(balanced_X)
+
+standardized_X = preprocessing.scale(balanced_X)
+
+model = SVC()
+y_pred = cross_val_predict(model, standardized_X, balanced_y, cv=5)
+print(metrics.classification_report(balanced_y, y_pred))
+print(metrics.confusion_matrix(balanced_y, y_pred))
