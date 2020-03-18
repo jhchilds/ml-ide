bfde1695a670f54c38d06b1a324d409c69e21d60
joshuachilds <jhchilds@uvm.edu>
2020-03-18 17:26:09 -0400

    seperated target from dataset after balancing but before saving to binary

--- create_features.py
+++ create_features.py
@@ -1,4 +1,5 @@
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import preprocessing
 import json
 import numpy as np
 
@@ -8,12 +9,35 @@ target = [target["target"] for target in data_list]
 
 vectorizer = CountVectorizer(analyzer="char", ngram_range=(2, 2), max_features=100)
 vectorized_X = vectorizer.fit_transform(corpus)
-print(vectorizer.get_feature_names())
-features = vectorized_X.toarray()
+
+X = vectorized_X.toarray()
+
 target = np.asarray(target)
-target = target.reshape((len(target), 1))
+target = target.reshape((len(target), 1)) # column vector
+data_with_labels = np.append(X, target, axis=1)
 
-X = np.append(features, target, axis=1)
-print(X)
-np.save("vectorized", X)
+balanced_X = np.zeros((1000, 100))
+balanced_y = np.zeros(1000, dtype="str")
 
+counts = {}
+
+index = 0
+for row in data_with_labels:
+    label = row[-1]
+    if label in counts:
+        counts[label] += 1
+    else:
+        counts[label] = 1
+
+    if counts[label] <= 500:
+        balanced_X[index] = row[:-1]
+        balanced_y[index] = row[-1]
+        index += 1
+
+print(counts)
+print(balanced_X)
+
+standardized_X = preprocessing.scale(balanced_X)
+
+np.save("vectorized_X", standardized_X)
+np.save("vectorized_y", balanced_y)
--- predict.py
+++ predict.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 from sklearn import metrics
-from sklearn import preprocessing
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
@@ -13,34 +12,16 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import ShuffleSplit
 from sklearn.model_selection import learning_curve
 
-data = np.load("vectorized.npy")
-print(data)
-print(data.shape)
+X = np.load("vectorized_X.npy")
+y = np.load("vectorized_y.npy")
+print(X)
+print(X.shape)
+print(y)
+print(y.shape)
 
-balanced_X = np.zeros((1000, 100))
-balanced_y = np.zeros(1000, dtype="str")
 
-counts = {}
-
-index = 0
-for row in data:
-    label = row[-1]
-    if label in counts:
-        counts[label] += 1
-    else:
-        counts[label] = 1
-
-    if counts[label] <= 500:
-        balanced_X[index] = row[:-1]
-        balanced_y[index] = row[-1]
-        index += 1
-
-print(counts)
-print(balanced_X)
-
-standardized_X = preprocessing.scale(balanced_X)
 
 model = SVC()
-y_pred = cross_val_predict(model, standardized_X, balanced_y, cv=5)
-print(metrics.classification_report(balanced_y, y_pred))
-print(metrics.confusion_matrix(balanced_y, y_pred))
+y_pred = cross_val_predict(model, X, y, cv=5)
+print(metrics.classification_report(y, y_pred))
+print(metrics.confusion_matrix(y, y_pred))
