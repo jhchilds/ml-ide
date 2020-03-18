18edfd67253b8c2cd925a0fd93dd7e9feb39dbd4
Sam Clark <sam.clark1142@gmail.com>
2020-03-18 19:38:36 -0400

    balance at the beginning not the end

--- create_data.py
+++ create_data.py
@@ -7,11 +7,22 @@ REPOS_DIR = "repos-cloned"
 data = []
 file_types = ["py", "java"]
 
+MAX_SAMPLES = 100000
+counts = {}
+index = 0
+
 for root, dirs, files in os.walk(REPOS_DIR):
     for name in files:
         file_path = os.path.join(root, name)
         for file_type in file_types:
             if file_path.endswith("." + file_type):
+                if file_type in counts:
+                    counts[file_type] += 1
+                else:
+                    counts[file_type] = 1
+                if counts[file_type] > MAX_SAMPLES:
+                    continue
+
                 try:
                     print(file_path)
                     with open(file_path) as source_file:
--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn import metrics
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import preprocessing
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
@@ -12,16 +13,16 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import ShuffleSplit
 from sklearn.model_selection import learning_curve
 
-X = np.load("vectorized_X.npy")
-y = np.load("vectorized_y.npy")
-print(X)
-print(X.shape)
-print(y)
-print(y.shape)
+data = np.load("vectorized.npy")
+print(data)
+print(data.shape)
 
+X = data[:, :-1]
+y = data[:, -1]
 
+standardized_X = preprocessing.scale(X)
 
 model = SVC()
-y_pred = cross_val_predict(model, X, y, cv=5)
+y_pred = cross_val_predict(model, standardized_X, y, cv=5)
 print(metrics.classification_report(y, y_pred))
 print(metrics.confusion_matrix(y, y_pred))
