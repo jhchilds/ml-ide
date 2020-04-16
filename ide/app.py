4b68ae578c444baad52d4ca87e71ca6995278981
joshuachilds <jhchilds@uvm.edu>
2020-04-16 09:49:56 -0400

    configuring app.py with flask-codemirror demo

--- ide/app.py
+++ ide/app.py
@@ -1,14 +1,36 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
+from flask_codemirror import CodeMirror
 
-app = Flask(__name__)
-app.config.from_mapping(
-    SECRET_KEY='dev',
+from flask_wtf import FlaskForm
+from flask_codemirror.fields import CodeMirrorField
+from wtforms.fields import SubmitField
+
+
+class MyForm(FlaskForm):
+    source_code = CodeMirrorField(language='python',
+                                config={'lineNumbers' : 'true'})
+    submit = SubmitField('Submit')
+
+
+SECRET_KEY = 'secret!'
+# mandatory
+CODEMIRROR_LANGUAGES = ['python', 'html']
+# optional
+CODEMIRROR_THEME = '3024-day'
+CODEMIRROR_ADDONS = (
+     ('display','placeholder'),
 )
 
+app = Flask(__name__)
+app.config.from_object(__name__)
+codemirror = CodeMirror(app)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    form = MyForm()
+    if form.validate_on_submit():
+        text = form.source_code.data
+    return render_template('index.html', form=form)
 
 if __name__ == "__main__":
-  app.run()
+  codemirror.init_app(app)
