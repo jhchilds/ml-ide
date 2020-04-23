b2b56e3bc93515157704f0f8ce789f2cac19a309
joshuachilds <jhchilds@uvm.edu>
2020-04-23 14:27:19 -0400

    codemirror alone checkpoint

--- ide/static/socket-controller.js
+++ ide/static/socket-controller.js
@@ -5,9 +5,35 @@ function getEl(id) {
 }
 const editor = getEl("codeForm");
 
+
+
+
 editor.addEventListener("keyup", (evt) => {
+
+    var cursorPosBeforeKeyup = editor_for_source_code.getCursor();
+
+    var lineBeforeKeyup = cursorPosBeforeKeyup['line'];
+    var charBeforeKeyup = cursorPosBeforeKeyup['ch']-1;
+
+    var cursorPosAfterKeyup = editor_for_source_code.getCursor();
+    var lineAfterKeyup = cursorPosAfterKeyup['line'];
+    var charAfterKeyup = cursorPosAfterKeyup['ch'];
+
+    var textToReplace = editor_for_source_code.getRange(cursorPosBeforeKeyup, cursorPosAfterKeyup);
+
+    var data = {
+        'lineBeforeKeyup': lineBeforeKeyup,
+        'charBeforeKeyup': charBeforeKeyup,
+        'lineAfterKeyup' :  lineAfterKeyup,
+        'charAfterKeyup' :  charAfterKeyup,
+        'textToReplace'  :  textToReplace,
+    };
+
+    console.log(charBeforeKeyup);
+
     const text = editor_for_source_code.getValue();
     socket.emit( 'code_text' , text);
+
 });
 
 socket.on('code_text', (data) => {
