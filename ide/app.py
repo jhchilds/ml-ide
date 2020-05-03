e36141a711d61c9f84b6edc03a7a40b488f482c0
joshuachilds <jhchilds@uvm.edu>
2020-05-03 18:13:54 -0400

    receiving code from client to be classifed

--- ide/app.py
+++ ide/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
@@ -8,5 +8,12 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 @app.route('/', methods=['GET','POST'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/test/', methods=['GET','POST'])
+def test():
+    if request.method == "POST":
+        print(request.form["code"])
+    return 'return'
 
 socketio.run(app, host="0.0.0.0", log_output=True)
