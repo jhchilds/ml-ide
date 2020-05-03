2109217079d22db3356c57a3b10cff90f8000f0c
joshuachilds <jhchilds@uvm.edu>
2020-05-03 18:12:57 -0400

    implemented timed ajax POST to send code to server when user stops typing

---
+++ ide/static/classify-editor.js
@@ -0,0 +1,23 @@
+var timeBetweenKeystrokes = 500;
+var timer;
+var $input = $('#firepad-container');
+
+$input.on('keydown', function () {
+    clearTimeout(timer);
+});
+
+$input.on('keyup', function () {
+    clearTimeout(timer);
+    timer = setTimeout(sendCode, timeBetweenKeystrokes);
+});
+
+function sendCode() {
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:5000/test/",
+        dataType: "json",
+        data: {
+            code: codeMirror.getValue()
+        }
+    });
+}
