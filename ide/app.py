72c7066d129a5b7bc319ebeac6e18f7501144404
joshuachilds <jhchilds@uvm.edu>
2020-05-03 16:36:24 -0400

    remove unused socket route

--- ide/app.py
+++ ide/app.py
@@ -5,15 +5,8 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-
 @app.route('/', methods=['GET','POST'])
 def index():
     return render_template('index.html')
-
-
-@socketio.on('code_text')
-def handle_code_text(message):
-    socketio.emit('code_text', message, broadcast=True)
-
 
 socketio.run(app, host="0.0.0.0", log_output=True)
