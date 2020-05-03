8e285943dee578fc817ed4b1f5ef23f74bd70094
joshuachilds <jhchilds@uvm.edu>
2020-05-03 16:33:25 -0400

    remove flask-codemirror from app.py

--- ide/app.py
+++ ide/app.py
@@ -1,30 +1,8 @@
 from flask import Flask, render_template, request
-from flask_codemirror import CodeMirror
 from flask_socketio import SocketIO
-
-from flask_wtf import FlaskForm
-from flask_codemirror.fields import CodeMirrorField
-from wtforms.fields import SubmitField
-
-
-class MyForm(FlaskForm):
-    source_code = CodeMirrorField(language='python',
-                                config={'lineNumbers' : 'true'})
-    submit = SubmitField('Submit')
-
-
-SECRET_KEY = 'secret!'
-# mandatory
-CODEMIRROR_LANGUAGES = ['python', 'html']
-# optional
-CODEMIRROR_THEME = '3024-day'
-CODEMIRROR_ADDONS = (
-     ('display','placeholder'),
-)
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-codemirror = CodeMirror(app)
 socketio = SocketIO(app, cors_allowed_origins='*')
 
 
