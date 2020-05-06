dbcf807f435ed5c41edde3490cf73212c41488de
joshuachilds <jhchilds@uvm.edu>
2020-05-06 02:55:13 -0400

    get_params for subprocess

--- ide/run.py
+++ ide/run.py
@@ -1,12 +1,18 @@
 import subprocess
 
-def run(code, lang):
-    filename =  f"editor_code.{lang}"
-    f = open(filename, "w").write(code)
 
-    comp = subprocess.run(["python3.8", filename],
+def _get_params(lang, filename):
+    return {
+        'py': ["python3.8", filename],
+    }.get(lang)
+
+
+def run(code, lang):
+    filename = f"editor_code.{lang}"
+    f = open(filename, "w").write(code)
+    comp = subprocess.run(_get_params(lang, filename),
                           stdout=subprocess.PIPE, encoding="UTF-8")
     output = comp.stdout
     return output
 
-print(run("print('hello')", 'py'))
+# python, haskell, java, javascript, c, swift
