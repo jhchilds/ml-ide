e44987681028f97afa6b58588dc185683edaab4f
joshuachilds <jhchilds@uvm.edu>
2020-05-06 02:19:42 -0400

    added route for interpreting/compiling editor code

--- ide/app.py
+++ ide/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for
 from flask_socketio import SocketIO
 import json
 from predict import predict
+from run import run
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -13,10 +14,16 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/classify/', methods=['POST'])
+@app.route('/classify', methods=['POST'])
 def classify():
     text = request.form["code"]
     return json.dumps({'lang': predict(text)})
 
+@app.route('/run', methods=['POST'])
+def run_code():
+    code = request.form["code"]
+    print ("DEBUG:", request.form)
+    lang = request.form["lang"]
+    return json.dumps({"output" : run(code, lang)})
 
-socketio.run(app, host="0.0.0.0", log_output=True)
+socketio.run(app, host="0.0.0.0", log_output=True, debug=True)
